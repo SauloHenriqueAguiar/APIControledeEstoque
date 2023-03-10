e8088428248dd15cd217e7668aaad716b10d27f9
Saulo Henrique <saulohjr@hotmail.com>
2023-03-09 21:11:54 -0300

    cm2

--- src/routes/usuario.route.js
+++ src/routes/usuario.route.js
@@ -3,18 +3,19 @@ const usuarioValidator = require('../validators/usuario.validator')
 const router = express.Router();
 const usuarioController = require('../controllers/usuario.controller');
 const { encontrarPorId } = require('../validators/usuario.validator');
+const verifyJWT = require('../middewares/authorizator');
 
 router.post('/',usuarioValidator.criar(),usuarioController.criar);
 
 router.post('/login',usuarioValidator.login(),usuarioController.login);
 
-route.get('/',usuarioController.encontrarTodos);
+route.get('/',verifyJWT,usuarioController.encontrarTodos);
 
-route.get('/:id',usuarioValidator.encontrarPorId(),usuarioController.encontrarporid);
+route.get('/:id',verifyJWT,usuarioValidator.encontrarPorId(),usuarioController.encontrarporid);
 
-router.put('/:id',usuarioValidator.atualizar(),usuarioController.atualizar);
+router.put('/:id',verifyJWT,usuarioValidator.atualizar(),usuarioController.atualizar);
 
-route.delete('/:id',usuarioValidator.deletar(),usuarioController.deletar);
+route.delete('/:id',verifyJWT,usuarioValidator.deletar(),usuarioController.deletar);
 
 module.exports = router;
 
